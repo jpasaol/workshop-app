2f7ded53807c93ce6c581fe7e5a216ce338acf3a
Jon Kane Pasaol <jpasaol@corebridge.net>
2021-02-24 01:45:48 +0800

    Updated initial styling to dashboard and updated path to 'dashboard' instead of 'home'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
-    path: 'home',
+    path: 'dashboard',
     loadChildren: () =>
       import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   }
